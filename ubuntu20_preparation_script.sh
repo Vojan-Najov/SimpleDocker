da1c4174ad186ce30d5c8a39f5ff4b502e989940
Vojan-Najov <serejah.mashichev@gmail.com>
2023-09-23 14:27:03 +0300

    Fix preparation script

--- ubuntu20_preparation_script.sh
+++ ubuntu20_preparation_script.sh
@@ -30,5 +30,18 @@ sudo apt install openbox
 sudo apt install xinit 
 sudo apt install kitty firefox-esr
 sudo apt install firefox
+sudo apt install gcc
+sudo apt install libfcgi-dev
+sudo apt install spawn-fcgi
+sudo apt install nginx
+sudo systemctl stop nginx.service
+sudo systemctl disable nginx.service
+sudo apt install docker-compose
 
+VERSION=$(
+ curl --silent "https://api.github.com/repos/goodwithtech/dockle/releases/latest" | \
+	  grep '"tag_name":' | \
+	   sed -E 's/.*"v([^"]+)".*/\1/' \
+   ) && curl -L -o dockle.deb https://github.com/goodwithtech/dockle/releases/download/v${VERSION}/dockle_${VERSION}_Linux-64bit.deb
+sudo dpkg -i dockle.deb && rm dockle.deb
 
